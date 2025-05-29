2803837489f2a8f5f3e3f1f07aa4f4f26a9541e7
JuanFer1996 <juan_andres_fer@hotmail.com>
2025-05-29 17:02:44 -0300

    ejer1.2

---
+++ Guia 9.1/Ejercicio1.2/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Ejercicio1._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] libreta = new int[60];
+            double[] notas = new double[60];
+            string[] nombres= new string[60];
+
+            Console.WriteLine("Ingrese numero de libreta");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int contador = 0;
+            while (n!= - 1){
+                libreta[contador]=n; 
+                Console.WriteLine("INgrese nombre");
+                nombres[contador] = Console.ReadLine;
+                Console.WriteLine("ingrese nota");
+                notas[contador]=Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Ingrese numero de libreta");
+                int n = Convert.ToInt32(Console.ReadLine());
+                contador++;
+            }
+            
+
+
+
+        }
+    }
+}
---
+++ Guia 9.1/Ejercicio1/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace Ejercicio1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int contador = 0;
+            int acumulador = 0;
+            int max;
+            int indicemax = 0;
+            int min;
+            int indicemin = 0;
+            int n = 0;
+            int[] numeros = new int[100];
+            Console.WriteLine("ingrese un numero, -1 para salir");
+            n = Convert.ToInt32(Console.ReadLine());
+            min = n;
+            max = n;
+            while (n != -1 || contador == 100)
+            {
+                numeros[contador] = n;
+                Console.WriteLine("ingrese un numero,-1 para salir");
+                n = Convert.ToInt32(Console.ReadLine());
+
+                contador++;
+
+
+            }
+            if (contador > 1)
+            {
+                for (int i = 0; i < contador; i++)
+                {
+                    acumulador += numeros[i];
+                    if (numeros[i] > max)
+                    {
+                        max = numeros[i];
+                        indicemax = i;
+                    }
+                    if (numeros[i] < min)
+                    {
+                        min = numeros[i];
+                        indicemin = i;
+                    }
+                }
+
+                for (int i = 0; i < contador; i++)
+                {
+                    Console.WriteLine($"{i}:{numeros[i]}");
+
+                }
+
+
+                double prom = 1.0 * acumulador / contador;
+                Console.WriteLine($"Promedio {prom}");
+                Console.WriteLine($"Valor maximo: {max} con indice {indicemax}");
+                Console.WriteLine($"Valor minimo: {min} con indice {indicemin}");
+            }
+        }
+    }
+}
